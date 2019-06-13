4f6e1753d365dbda7bf7f9796cf8b2c49c047a07
igorgo-hyuna <igor.gologots@hyuna.bb>
2019-06-13 14:20:24 +0300

    Added new sections2

--- app/js/common.js
+++ app/js/common.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
-	/* Parallax 
-	$('.parallax-window').parallax({imageSrc: 'img/background.jpg'}); */
 
-	/* Resize
-	function heightDetect(){
-		$(".parallax-window").css("height", $(window).height());	
-	}
-	heightDetect();
-	$(window).resize(function(){
-		heightDetect();
-	});
-  */
+  /* Counter */
+  $('.number-box').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  /* Resize */
+  function heightDetect(){
+    $("header").css("height", $(window).height());	
+  }
+  heightDetect();
+  $(window).resize(function(){
+    heightDetect();
+  });
 
   /* Menu animate */
   $(".horizontal .sub-menu").addClass("fadeInLeft animated");
@@ -18,30 +21,62 @@ $(document).ready(function() {
   /* Burger */
   $(".toggle_mnu").click(function() {
     $(".sandwich").toggleClass("active");
-});
+  });
 
   /* Burger. Hover */
   $(".top_mnu ul a").click(function() {
     $(".top_mnu").fadeOut(600);
     $(".sandwich").toggleClass("active");
     $(".top_text").css("opacity", "1");
-}).append("<span>");
+  }).append("<span>");
 
   /* Burger. Open and close */
   $(".toggle_mnu").click(function() {
     if ($(".top_mnu").is(":visible")) {
-        $(".top_text").css("opacity", "1");
-        $(".top_mnu").fadeOut(600);
-        $(".top_mnu li a").removeClass("fadeInUp animated");
+      $(".top_text").css("opacity", "1");
+      $(".top_mnu").fadeOut(600);
+      $(".top_mnu li a").removeClass("fadeInUp animated");
     } else {
-        $(".top_text").css("opacity", ".1");
-        $(".top_mnu").fadeIn(600);
-        $(".top_mnu li a").addClass("fadeInUp animated");
+      $(".top_text").css("opacity", ".1");
+      $(".top_mnu").fadeIn(600);
+      $(".top_mnu li a").addClass("fadeInUp animated");
     };
-});
+  });
 
   /* Carousel */
   $('.carousel').carousel({
-      interval: 98000
+    interval: 8000
   })
+
+  /*Scrool animation*/
+  $(window).scroll(function() {
+
+    $('.mov').each(function() {
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow + 500) {
+        $(this).addClass('fadeIn animated');
+        $(this).css('opacity', '1');
+      }
+    });
+
+    $('.mov-left').each(function() {
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow + 500) {
+        $(this).addClass('fadeInLeft animated');
+        $(this).css('opacity', '1');
+      }
+    });
+
+    $('.mov-right').each(function() {
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow + 500) {
+        $(this).addClass('fadeInRight animated');
+        $(this).css('opacity', '1');
+      }
+    });
+  });
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -28,8 +28,8 @@ gulp.task('scripts', function(){
 		// 'app/libs/parallax.js-1.5.0/parallax.min.js',
 		// 'app/libs/pageScrool2/PageScroll2id.min.js',
 		// 'app/libs/owlCarousel2-2.3.4/dist/owl.carousel.min.js',
-		// 'app/libs/jqueryCounter/counter.lib.js',
-		// 'app/libs/jqueryCounter/jquery.counterup.min.js',
+		'app/libs/jqueryCounter/counter.lib.js',
+		'app/libs/jqueryCounter/jquery.counterup.min.js',
 		// 'app/libs/backgroundvideo/jquery.backgroundvideo.min.js',
 	])
 	.pipe(concat('libs.min.js'))
